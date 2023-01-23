6a49ad3e72e31e87c3df1a70d0681949043d9a13
Paloma Wink <paloma.wink@hotmail.com>
2023-01-23 15:32:03 -0300

    Operadores lógicos parte 1 e 2

---
+++ Fundamentos/Secao-03-Introducao-ao-JavaScript/Operadores-logicos-pt2.js
@@ -0,0 +1,8 @@
+let weekDay = 'quarta-feira'
+
+if (weekDay == 'segunda-feira'|| weekDay == 'terça-feira' || weekDay == 'quarta-feira' || weekDay == 'quinta-feira' || weekDay == 'sexta-feira'){
+    console.log ('Obam mais um dia de aprendizado na Trybe >:D');
+}
+else {
+    console.log ('FINALMENTE, descanso merecido UwU');
+}

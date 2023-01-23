fbd827beb322a46c8eacaf4e312fa15598561b1d
Paloma Wink <paloma.wink@hotmail.com>
2023-01-23 13:56:16 -0300

    Condições If/Else

---
+++ Fundamentos/Secao-03-Introducao-ao-JavaScript/Condicao-if-else.js
@@ -0,0 +1,11 @@
+const notaCandidato = 70;
+
+if (notaCandidato >= 80){
+    console.log ('Parabéns, você foi aprovado!');
+}
+else if (notaCandidato < 80 && notaCandidato >= 60){
+    console.log ('Você está na lista de espera.');
+}
+else {
+    console.log ('Infelizmente, você reprovou.')
+}

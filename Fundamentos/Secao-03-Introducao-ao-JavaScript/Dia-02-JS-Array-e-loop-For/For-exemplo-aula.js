ded69eceb38c115dabfc8ab88f113c6616741f72
Paloma Wink <paloma.wink@hotmail.com>
2023-01-24 11:38:47 -0300

    Exercicios do For

---
+++ Fundamentos/Secao-03-Introducao-ao-JavaScript/Dia-02-JS-Array-e-loop-For/For-exemplo-aula.js
@@ -0,0 +1,11 @@
+let num = 7;
+
+for (let index = 1; index <= 9; index += 1){
+    console.log (num * index);
+}
+
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+
+for (let index = 0; index < listaDeNomes.length; index += 1){
+    console.log ('Boas vindas, ' + listaDeNomes[index] + '!') 
+}

a91e582c66f156f249505f669971f3a5ac31bee2
Paloma Wink <paloma.wink@hotmail.com>
2023-01-26 19:03:56 -0300

    Finalização dos exercicios da aula de ontem e conclusão dos exercicios de objetos

--- Fundamentos/Secao-03-Introducao-ao-JavaScript/Dia-03-Logica-de-programacao/maior-palavra-menor-palavra.js
+++ Fundamentos/Secao-03-Introducao-ao-JavaScript/Dia-03-Logica-de-programacao/maior-palavra-menor-palavra.js
@@ -9,10 +9,37 @@ let array = ['java', 'javascript', 'python', 'html', 'css'];
 
 - Imprimir a menor palavra
 */
-let maiorPalavra = [];
+let maiorPalavra = array[0];
 
-for (let index = 0; index <= array.length; index += 1){
-    for (let index2 = 0; index2 >= array[index].length; index += 1){
-        if (array[index].length)
+for (let index = 0; index < array.length; index += 1){
+        if (array[index].length > maiorPalavra.length){
+            maiorPalavra = array[index];
+        }
+}
+console.log(maiorPalavra);
+
+let menorPalavra = array[0];
+
+for (let index = 0; index < array.length; index += 1){
+        if (array[index].length < menorPalavra.length){
+            menorPalavra = array[index];
+        }
+}
+console.log(menorPalavra);
+
+//Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
+
+let maiorPrimo = 0;
+
+for (let i = 2; i <= 50; i += 1){
+    let ePrimo = true;
+    for (let j = 2; j < i; j += 1){
+        if (i % j == 0){
+            ePrimo = false;
+        }
+    }
+    if (ePrimo){
+        maiorPrimo = i;
     }
 }
+console.log(maiorPrimo);

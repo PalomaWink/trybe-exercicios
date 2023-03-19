c828eb6ab321f32cbcb3e89cb47e6dc09e163669
Paloma Wink <paloma.wink@hotmail.com>
2023-03-19 19:48:28 -0300

    Segundo exercicio sobre ambiente de desenvolvimento com teste do surge

--- Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/Exercicio-2/src/script.js
+++ Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/Exercicio-2/src/script.js
@@ -1,1 +1,17 @@
 import './style.css';
+import validator from 'validator';
+
+const inputField = document.querySelector('#value');
+const selectField = document.querySelector('#options');
+const btnValidation = document.querySelector('#button');
+const textField = document.querySelector('#answer')
+
+btnValidation.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const campos = {
+        cpf: validator.isTaxID(inputField.value, 'pt-BR'),
+        email: validator.isEmail(inputField.value),
+    };
+    textField.innerHTML = `A validação retornou ${campos[selectField.value]}`
+})

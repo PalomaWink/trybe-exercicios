99506297c5416d147ecc14f3913f96ac83fe625d
Paloma Wink <paloma.wink@hotmail.com>
2023-03-19 15:12:47 -0300

    Continuando exercicio sobre criar um ambiente de desenvolvimento

--- Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/src/script.js
+++ Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/src/script.js
@@ -1,3 +1,8 @@
 import { nanoid } from 'nanoid'
 
-console.log(nanoid());
+const passwordBtEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtEl.addEventListener('click', () => {
+    displayPasswordEl.innerHTML = nanoid()
+})

3bd7051b1b4c7cf8a796ee0ba2aa3f31d3cd7ef5
Paloma Wink <paloma.wink@hotmail.com>
2023-03-19 16:04:53 -0300

    Estilizando a aplicação

--- Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/src/script.js
+++ Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/src/script.js
@@ -1,8 +1,9 @@
 import { nanoid } from 'nanoid'
+import './style.css'
 
 const passwordBtEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
 
 passwordBtEl.addEventListener('click', () => {
-    displayPasswordEl.innerHTML = nanoid()
-})
+    displayPasswordEl.innerHTML = nanoid();
+});

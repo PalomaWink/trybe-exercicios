a7e8a74f0fdebcceca30f0c212885d5e1d148292
Paloma Wink <paloma.wink@hotmail.com>
2023-03-19 16:27:38 -0300

    Adicionando biblioteca copy

--- Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/src/script.js
+++ Front-end/Secao-01-Introducao-ao-front-end-e-JS-assincrono/Dia-01-Ambiente-de-desenvolvimento/src/script.js
@@ -1,9 +1,16 @@
-import { nanoid } from 'nanoid'
-import './style.css'
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+import './style.css';
 
 const passwordBtEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
+const copyBtn = document.getElementsByClassName('copy')[0];
 
 passwordBtEl.addEventListener('click', () => {
     displayPasswordEl.innerHTML = nanoid();
+});
+
+copyBtn.addEventListener('click', () => {
+    copy(displayPasswordEl.innerText);
+    alert('Senha copiada!');
 });
